e015872cefa22064284ac9961d0f4c8c54a1c3aa
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-11 20:59:18 -0300

    test: presence of validations and uniqueness validation #8

--- spec/models/hospede_spec.rb
+++ spec/models/hospede_spec.rb
@@ -8,6 +8,36 @@ RSpec.describe Hospede, type: :model do
       it{ should have_db_column(:celular).of_type(:string)}
       it{ should have_db_column(:email).of_type(:string)}
     end
+
+    context "validations" do
+
+      context "presence of" do
+        it "nome" do
+          hospede = FactoryBot.build(:hospede, nome:nil)
+          expect(hospede).not_to be_valid 
+        end
+        it "cpf" do
+          hospede = FactoryBot.build(:hospede, cpf:nil)
+          expect(hospede).not_to be_valid 
+        end
+        it "celular" do
+          hospede = FactoryBot.build(:hospede, celular:nil)
+          expect(hospede).not_to be_valid 
+        end
+        it "email" do
+          hospede = FactoryBot.build(:hospede, email:nil)
+          expect(hospede).not_to be_valid 
+        end
+      end
+      
+      context "uniqueness of" do
+        subject { FactoryBot.build(:hospede) } 
+        it { should validate_uniqueness_of(:cpf) }
+      end
+      
+    end
   end
+
+
   
 end
