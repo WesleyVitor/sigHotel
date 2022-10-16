b7c48d38cc72b88ebfc2fa6566dcfd7549681c33
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-16 13:15:15 -0300

    test: using tdd to show action #8

--- spec/requests/hospedes_spec.rb
+++ spec/requests/hospedes_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe "Hospedes", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /show" do
+    it "return 200 status" do
+      endereco = FactoryBot.create(:endereco)
+      hospede = FactoryBot.create(:hospede, endereco_id: endereco)
+      get hospede_path(hospede.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
