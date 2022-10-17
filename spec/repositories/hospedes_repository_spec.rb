0f938eddd1f9cc7512b9e8e9c88f209b02271b27
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-17 09:48:32 -0300

    test: Método get_hospede_by_id using tdd #8

--- spec/repositories/hospedes_repository_spec.rb
+++ spec/repositories/hospedes_repository_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe 'HospedesRepository', type: :repository do
             expect(hospedes).to eq(Hospede.all)
         end
     end
+
+    context "method get_hospede_by_cpf" do
+        it "should return especific hospede" do
+            hospede_created = FactoryBot.create(:hospede)
+            hospede = HospedeRepository.get_hospede_by_id(hospede_created.id)
+            expect(Hospede.last).to  eq(hospede)
+        end
+
+    end
     
     
 
