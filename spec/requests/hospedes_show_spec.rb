c520a709b4027a342f5f36deaafa706e225be2e9
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-17 20:03:08 -0300

    test: Body of response in show action in HospedeController #8

--- spec/requests/hospedes_show_spec.rb
+++ spec/requests/hospedes_show_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 RSpec.describe "HospedesShow", type: :request do
   
   describe "GET /:id (hospedes)" do
-    it "return 200 status" do
+    let(:hospede){FactoryBot.create(:hospede)}
+    it " should return 200 status" do
       
-      hospede = FactoryBot.create(:hospede)
+      
       get hospede_path(hospede.id)
       expect(response).to have_http_status(200)
+      
+    end
+
+    it "should include hospede created in body" do
+      get hospede_path(hospede.id)
+      expect(response.body).to include(hospede.nome) 
+      expect(response.body).to include(hospede.email) 
+      expect(response.body).to include(hospede.celular) 
       
     end
   end
