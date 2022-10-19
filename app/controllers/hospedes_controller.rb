7b3a3ca80a897e24529502d6a535172cee2d116c
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-19 10:21:44 -0300

    feat: Action create from HospedeController #8

--- app/controllers/hospedes_controller.rb
+++ app/controllers/hospedes_controller.rb
@@ -1,11 +1,38 @@
 require 'hospede_repository.rb'
 class HospedesController < ApplicationController
+    before_action :get_hospede, only: [:show]
     def index
         @hospedes = HospedeRepository.get_all_hospedes
     end
 
 
     def show
+        
+    end
+
+    def new
+        @hospede = Hospede.new
+    end
+
+    def create
+        @hospede = Hospede.new(valid_params)
+        
+        if @hospede.save
+            redirect_to hospedes_path, notice:"Hospede was successfully created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+
+
+    private
+    def valid_params
+
+        params.require(:hospede).permit(:nome, :cpf, :email, :celular, :status)
+    end
+
+    def get_hospede
         @hospede = HospedeRepository.get_hospede_by_id(params[:id])
     end
 end
