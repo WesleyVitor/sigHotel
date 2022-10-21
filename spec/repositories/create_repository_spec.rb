6000a513d65f3130197fe235da55a8927c04180d
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-21 10:16:59 -0300

    test: HospedeRepository method create_hospede with invalid attributes #8

--- spec/repositories/create_repository_spec.rb
+++ spec/repositories/create_repository_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe 'HospedeCreate', type: :repository do
             }.to change(Hospede, :count).by(1)
         end
     end
+    context "invalid params past" do
+        it "should not create a new hospede when not past nome attribute" do
+            params = FactoryBot.attributes_for(:hospede, nome: nil)
+            expect {
+                HospedeRepository.create_hospede(params)
+            }.to_not change(Hospede, :count)
+        end
+        it "should not create a new hospede when not past email attribute" do
+            params = FactoryBot.attributes_for(:hospede, email: nil)
+            expect {
+                HospedeRepository.create_hospede(params)
+            }.to_not change(Hospede, :count)
+        end
+        it "should not create a new hospede when not past celular attribute" do
+            params = FactoryBot.attributes_for(:hospede, celular: nil)
+            expect {
+                HospedeRepository.create_hospede(params)
+            }.to_not change(Hospede, :count)
+        end
+        it "should not create a new hospede when not past cpf attribute" do
+            params = FactoryBot.attributes_for(:hospede, cpf: nil)
+            expect {
+                HospedeRepository.create_hospede(params)
+            }.to_not change(Hospede, :count)
+        end
+    end
+
+
 end
