385724bcb76a81df3f6d493d0b7dbb2f43a10cda
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-24 10:07:45 -0300

    feat: Using enum to status attibute in Hospede Model #8

--- app/models/hospede.rb
+++ app/models/hospede.rb
@@ -4,5 +4,8 @@ class Hospede < ApplicationRecord
     
     validates :nome, :celular, :email, presence: true
     validates :cpf, presence: true, uniqueness: true
+
+    enum status: {active: "active", inactive: "inactive", reserve: "reserve"}
+
     has_one :endereco
 end
