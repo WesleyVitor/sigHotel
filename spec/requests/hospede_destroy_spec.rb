b68330af644e29ab407503cb67841ba5a3f2d33d
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-30 14:40:46 -0300

    test: When have not hospede in db #8

--- spec/requests/hospede_destroy_spec.rb
+++ spec/requests/hospede_destroy_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper.rb'
 
 RSpec.describe "HospedeDestroy", type: :request do
   context "when exist a hospede persisted" do
+    let(:hospede_created){ FactoryBot.create(:hospede)}
     it "should change status to inactive" do
-      hospede_created = FactoryBot.create(:hospede)
       delete hospede_path(hospede_created.id)
       hospede = Hospede.find(hospede_created.id)
       expect(hospede).to be_inactive
     end
   end
+
+  context "when not exist a hospede persisted" do
+    it "should hospede finded be nil" do
+      hospede = FactoryBot.build(:hospede, id:1)
+      delete hospede_path(hospede.id)
+      hospede = Hospede.find_by_id(hospede.id)
+      expect(hospede).to be_nil  
+    end
+  end
+  
+
 
 end
