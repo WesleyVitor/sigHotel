203d31c36b3a79ada8906b0f7d9e88a8aa1d3d95
WesleyVitor <wesleydemorais@outlook.com.br>
2022-10-30 14:40:16 -0300

    chore: Anottade dependence #8

--- spec/models/endereco_spec.rb
+++ spec/models/endereco_spec.rb
@@ -3,17 +3,25 @@
 # Table name: enderecos
 #
 #  id          :integer          not null, primary key
-#  rua         :string
-#  numero      :string
-#  complemento :string
 #  bairro      :string
-#  cidade      :string
-#  estado      :string
-#  pais        :string
 #  cep         :string
+#  cidade      :string
+#  complemento :string
+#  estado      :string
+#  numero      :string
+#  pais        :string
+#  rua         :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  hospede_id  :integer
+#
+# Indexes
+#
+#  index_enderecos_on_hospede_id  (hospede_id)
+#
+# Foreign Keys
+#
+#  hospede_id  (hospede_id => hospedes.id)
 #
 require 'rails_helper'
 
