86b60f6e8fdf3ba48345e91f905e3adcea126258
WesleyVitor <wesleydemorais@outlook.com.br>
2022-11-01 12:42:21 -0300

    refactor(test): show action of Hospede #8

--- spec/requests/hospedes_show_spec.rb
+++ spec/requests/hospedes_show_spec.rb
@@ -1,24 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "HospedesShow", type: :request do
-  
-  describe "GET /:id (hospedes)" do
+RSpec.describe "HospedesShow", type: :request do  
+  context "when exist hospede" do
     let(:hospede){FactoryBot.create(:hospede)}
     it " should return 200 status" do
-      
-      
       get hospede_path(hospede.id)
       expect(response).to have_http_status(200)
-      
     end
-
-    # it "should include hospede created in body" do
-    #   get hospede_path(hospede.id)
-    #   expect(response.body).to include(hospede.nome) 
-    #   expect(response.body).to include(hospede.email) 
-    #   expect(response.body).to include(hospede.celular) 
-      
-    # end
   end
 
+  context "when not exist hospede" do
+    it "should return some status" do
+      get hospede_path(1)
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+    
 end
